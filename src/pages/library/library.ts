e64bcaee4510aa62d063f3fc58fa66c4470c38be
Djavani Gomes <djavanirgomes@gmail.com>
2018-07-26 23:47:02 -0300

    Using the Quotes Data

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,25 +1,13 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component,OnInit } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
 
-/**
- * Generated class for the LibraryPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import { Quote } from './../../data/quote.interface';
 @IonicPage()
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
-export class LibraryPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
-  }
+export class LibraryPage implements OnInit {
+  quoteCollection: { category: string, quote: Quote[], icon: string}
 
 }
