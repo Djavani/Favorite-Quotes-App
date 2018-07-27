eaa59c1eab52c4532648a6b313a4616f966ad416
Djavani Gomes <djavanirgomes@gmail.com>
2018-07-27 00:04:40 -0300

    How to create more complex List Items

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -2,12 +2,16 @@ import { Component,OnInit } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 
 import { Quote } from './../../data/quote.interface';
+import quotes from '../../data/quotes';
 @IonicPage()
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
 export class LibraryPage implements OnInit {
-  quoteCollection: { category: string, quote: Quote[], icon: string}
-
+  quoteCollection: { category: string, quotes: Quote[], icon: string}[];
+  
+  ngOnInit(): void {
+    this.quoteCollection = quotes;
+  }
 }
